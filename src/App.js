ca2bc8bf9287313319bfc4825b7192634b0d1576
c-birdsey <calder.birdsey@gmail.com>
2019-11-06 09:50:57 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mode: 'landing',
+    };
+  }
+
+  render() {
+    let bodyStyle; 
+
+    //landing
+    if (this.state.mode === 'landing') {
+      bodyStyle = ("landing");
+    } else if(this.state.mode === 'questions') {
+      bodyStyle = ('questions');
+    }
+
+    return (
+      <div className={bodyStyle}>
+    
+      </div>
+    );
+  }
 }
 
 export default App;
